8fe01baa014917f69910eb7f644df884482f2077
dortaedward <dortaedward@gmail.com>
2023-09-24 14:06:03 -0400

    corrected typos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
-import PageHead from "@/components/head";
 import { getSession, signIn } from "next-auth/react";
-import Link from "next/link";
+// import Link from "next/link";
 import { MdStorage } from "react-icons/md"
 import BaseLayout from "@/layouts/DefaultLayout";
 import type { GetServerSidePropsContext } from "next";
@@ -18,7 +17,7 @@ export default function Home() {
         <main className="flex items-center justify-center h-[calc(100vh-66px)] flex-col px-6 py-4">
           <h1 className="text-gray-100 text-8xl font-bold text-center">Organize. Manage. Trade.</h1>
           <div className="my-4"></div>
-          <p className="text-gray-400 text-2xl max-w-4xl text-center">{"TCG Collection's services gives trading card game collectors the infrastructure that's needed to store and maintain their collectins and focilitate trades/sales."}</p>
+          <p className="text-gray-400 text-2xl max-w-4xl text-center">{"TCG Vault's services gives trading card game collectors the infrastructure that's needed to store and maintain their collectins and focilitate trades/sales."}</p>
           <div className="my-4"></div>
           <div className="flex gap-3">
             <button onClick={() => signIn()} className="text-neutral-950 text-lg flex items-center gap-2 bg-gray-100 hover:bg-gray-300 px-3 py-2 rounded-md"><MdStorage size={20} /> Start Storing</button>
